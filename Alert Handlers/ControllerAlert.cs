3f4083d35ff5bff87a4f2256c8d7c64636e5b896
navanee867 <navanee777@gmail.com>
2024-08-21 14:34:58 +0530

    Add files via upload

---
+++ Alert Handlers/ControllerAlert.cs
@@ -0,0 +1,16 @@
+﻿using conclusive.Entities;
+using conclusive.Interfaces;
+
+namespace conclusive
+{
+    public class ControllerAlert(ILogger logger) : IAlertTarget
+    {
+        private readonly ILogger _logger = logger;
+
+        public void Alert(BreachType breachType)
+        {
+            const ushort header = 0xfeed;
+            _logger.Log($"{header} : {breachType}");
+        }
+    }
+}
---
+++ Alert Handlers/EmailAlert.cs
@@ -0,0 +1,28 @@
+﻿using conclusive.Entities;
+using conclusive.Interfaces;
+
+namespace conclusive
+{
+    public class EmailAlert(ILogger logger) : IAlertTarget
+    {
+        private readonly ILogger _logger = logger;
+
+        public void Alert(BreachType breachType)
+        {
+            string recepient = "a.b@c.com";
+            switch (breachType)
+            {
+                case BreachType.TOO_LOW:
+                    _logger.Log($"To: {recepient}");
+                    _logger.Log("Hi, the temperature is too low");
+                    break;
+                case BreachType.TOO_HIGH:
+                    _logger.Log($"To: {recepient}");
+                    _logger.Log("Hi, the temperature is too high");
+                    break;
+                case BreachType.NORMAL:
+                    break;
+            }
+        }
+    }
+}
